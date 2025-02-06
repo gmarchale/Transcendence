9928e77c4ab92e0b311b125f5807cd720b3ba4fc
Zerrino <afds999@gmail.com>
2025-02-06 12:48:47 +0100

    okay

--- backend/livechat/views.py
+++ backend/livechat/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import status, generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -10,18 +11,21 @@ from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 import logging
 
 # Create your views here.
+User = get_user_model()
 
 @api_view(['GET'])
 @ensure_csrf_cookie
 @permission_classes([AllowAny])
 def test_view(request):
 
-    user_id = request.data.get('user_id')
+    user_id = request.query_params.get('user_id')
 
     if not user_id:
         return Response({
             'detail': 'Please provide user_id'
         }, status=status.HTTP_400_BAD_REQUEST)
-
-
-    return Response('Nice!')
+    try:
+        user = get_object_or_404(User, id=user_id)
+        return Response({'id': user.id, 'username': user.username, 'email': user.email}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
