de3b0e456a97297d460bbb73e3eb5b8e7435a835
Syden <je.tollenaers@gmail.com>
2025-02-16 21:25:31 +0100

    save

--- frontend/js/settings.js
+++ frontend/js/settings.js
@@ -37,8 +37,68 @@ async function initSettings(){
         document.getElementById("settings_lang_fr").classList.toggle("active", lang === "fr");
         document.getElementById("settings_lang_en").classList.toggle("active", lang === "en");
     }
+    await loadTranslations();
 
-	await loadTranslations();
+    let selectedFile = null;
+    let cropper = null;
+
+    document.getElementById("settings_avatar_upload").addEventListener("change", function(event) {
+        const file = event.target.files[0];
+
+        if (!file) return;
+        if (file.type !== "image/jpeg") {
+            alert("Veuillez sélectionner un fichier JPEG !"); // TODO
+            event.target.value = "";
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            selectedFile = file;
+            document.getElementById("settings_cropImage").src = e.target.result;
+            document.getElementById("settings_popupContainer").style.display = "block";
+
+            if (cropper)
+                cropper.destroy();
+            cropper = new Cropper(document.getElementById("settings_cropImage"), {
+                aspectRatio: 1,
+                viewMode: 2
+            });
+        };
+        reader.readAsDataURL(file);
+    });
+ 
+    document.getElementById("settings_confirmUpload").addEventListener("click", function() {
+        if (!cropper) return;
+
+        cropper.getCroppedCanvas().toBlob((blob) => {
+            const croppedFile = new File([blob], selectedFile.name, { type: "image/jpeg" });
+
+            const formData = new FormData();
+            formData.append("image", croppedFile);
+
+            // fetch("/api/upload/", {
+            //     method: "POST",
+            //     body: formData
+            // })
+            // .then(response => response.json())
+            // .then(data => {
+            //     alert("Image envoyée avec succès !");
+            //     document.getElementById("popupContainer").style.display = "none";
+            // })
+            // .catch(error => console.error("Erreur upload :", error));
+            // TODO
+        }, "image/jpeg");
+        alert("Image confirmée !"); // TODO
+        document.getElementById("settings_popupContainer").style.display = "none";
+    });
+
+    document.getElementById("settings_cancelUpload").addEventListener("click", function() {
+        alert("Image annulée !"); // TODO
+        document.getElementById("settings_avatar_upload").value = "";
+        document.getElementById("settings_popupContainer").style.display = "none";
+        selectedFile = null;
+    });
 }
 
 let translationsCache = null;
