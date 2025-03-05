f733e6f5f3e03c7b07dcf0c5c8db8a119dca7ccc
Syden <je.tollenaers@gmail.com>
2025-03-05 16:12:17 +0100

    patched navigation error

--- frontend/js/navigation.js
+++ frontend/js/navigation.js
@@ -11,7 +11,7 @@ async function loadContentFromHash() {
     });
     
     let loadPage = 1;
-    if(fullHash != "login" && fullHash != "register"){
+    if(mainPath != "login" && mainPath != "register"){
         let result = await loadHeader();
         if (result === false)
             loadPage = 0;
@@ -22,14 +22,14 @@ async function loadContentFromHash() {
 		document.getElementById("header").classList.remove("active");
 		document.getElementById("chat_main_container").classList.remove("active");
 	}
-    document.title = fullHash.charAt(0).toUpperCase() + fullHash.slice(1) + " - PONG";
+    document.title = mainPath.charAt(0).toUpperCase() + mainPath.slice(1) + " - PONG";
 	if(isPreloaded == 0 || loadPage == 0)
 		return;
 
-    let pageDiv = document.getElementById(fullHash);
+    let pageDiv = document.getElementById(mainPath);
     if (pageDiv) {
         pageDiv.classList.add("active");
-        switch (fullHash){
+        switch (mainPath){
             case "profile": loadProfile();break;
             case "settings": loadSettings();break;
             case "game": loadGame();break;
