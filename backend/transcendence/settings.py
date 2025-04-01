57015a259b6c2e8a03084ca001429c93a4412312
dell xps 15 <gaetan.marchale@hotmail.com>
2025-04-01 14:58:17 +0200

    .env

--- backend/transcendence/settings.py
+++ backend/transcendence/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pnt#+^a_*^s^t60+(#**q-vpg39mn697!iq9*q&p-1fr7u5gs&'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
